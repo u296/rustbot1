7a5df0470ace52ad93c44ed959a5269331469f03
u296 <u296b3g@gmail.com>
2021-04-18 18:30:38 +0200

    added buffered send from blocking iterator

--- src/utils/textchannel.rs
+++ src/utils/textchannel.rs
@@ -49,3 +49,38 @@ where
     }
     Ok(())
 }
+
+pub async fn send_buffered_blocking<'a, I>(
+    ctx: &Context,
+    channel: ChannelId,
+    mut lines: I,
+) -> Result<(), Box<dyn Error + Send + Sync>>
+where
+    I: Unpin + Iterator<Item = &'a str>,
+{
+    let mut output_buf = String::new();
+    let mut last_message_time = Instant::now();
+
+    while let Some(line) = lines.next() {
+        output_buf.push_str(&format!(
+            "{}\n",
+            String::from_utf8(strip_ansi_escapes::strip(&line)?)?
+        ));
+        if !output_buf.trim().is_empty()
+            && Instant::now() >= last_message_time + Duration::from_secs(2)
+        {
+            for chunk in output_buf.as_bytes().chunks(2000) {
+                channel.say(&ctx, std::str::from_utf8(chunk)?).await?;
+            }
+            last_message_time = Instant::now();
+            output_buf = String::new();
+        }
+    }
+    if !output_buf.trim().is_empty() {
+        for chunk in output_buf.as_bytes().chunks(2000) {
+            channel.say(&ctx, std::str::from_utf8(chunk)?).await?;
+            tokio::time::sleep(Duration::from_secs(2)).await;
+        }
+    }
+    Ok(())
+}
