137a8a303093c95f71797a7f24c3a02bc07a5b9b
u296 <u296b3g@gmail.com>
2021-04-21 11:22:44 +0200

    copy log file to .txt to give preview in discord

--- src/commands/debug.rs
+++ src/commands/debug.rs
@@ -11,6 +11,8 @@ use serenity::prelude::*;
 use crate::utils;
 
 const LOG_FILE_LOCATION: &'static str = "/home/discord/logs/rustbot1/current";
+const LOG_FILE_TMP_STORE_LOCATION: &'static str = "/tmp/rustbot/log.txt";
+const LOG_FILE_TMP_STORE_DIR: &'static str = "/tmp/rustbot";
 
 #[group]
 #[commands(show_latest_log)]
@@ -18,5 +20,12 @@ struct Debug;
 
 #[command]
 async fn show_latest_log(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
-    utils::send_text_file(ctx, msg.channel_id, vec![LOG_FILE_LOCATION].into_iter()).await
+    tokio::fs::create_dir_all(LOG_FILE_TMP_STORE_DIR).await?;
+    tokio::fs::copy(LOG_FILE_LOCATION, LOG_FILE_TMP_STORE_LOCATION).await?;
+    utils::send_text_file(
+        ctx,
+        msg.channel_id,
+        vec![LOG_FILE_TMP_STORE_LOCATION].into_iter(),
+    )
+    .await
 }
