7af6069d3992dacb23b6cb790036901159ce0258
u296 <u296b3g@gmail.com>
2021-04-28 00:57:46 +0200

    ct fix

--- src/commands/debug.rs
+++ src/commands/debug.rs
@@ -42,14 +42,14 @@ async fn list_server_members(ctx: &Context, msg: &Message, _args: Args) -> Comma
     let mut s = String::new();
 
     for member in memberlist {
-        s.push_str(format!(
+        s.push_str(&format!(
             "nickname: {:?}, name: {}, discriminator: {}",
             member.nick, member.user.name, member.user.discriminator
         ));
         s.push('\n')
     }
 
-    utils::send_buffered_text(ctx, msg.channel_id, futures::stream::iter(s.trim().lines()));
+    utils::send_buffered_text(ctx, msg.channel_id, futures::stream::iter(s.trim().lines())).await?;
 
     Ok(())
 }
