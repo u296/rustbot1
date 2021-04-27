6271435124ae4fc325c894e5bf4dfe907e215de5
u296 <u296b3g@gmail.com>
2021-04-28 00:56:33 +0200

    added member listing debug command

--- src/commands/debug.rs
+++ src/commands/debug.rs
@@ -15,7 +15,7 @@ const LOG_FILE_TMP_STORE_LOCATION: &'static str = "/tmp/rustbot/log.txt";
 const LOG_FILE_TMP_STORE_DIR: &'static str = "/tmp/rustbot";
 
 #[group]
-#[commands(show_latest_log)]
+#[commands(show_latest_log, list_server_members)]
 struct Debug;
 
 #[command]
@@ -28,4 +28,28 @@ async fn show_latest_log(ctx: &Context, msg: &Message, _args: Args) -> CommandRe
         vec![LOG_FILE_TMP_STORE_LOCATION].into_iter(),
     )
     .await
+}
+
+#[command]
+async fn list_server_members(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
+    let memberlist = msg
+        .guild(ctx)
+        .await
+        .unwrap()
+        .members(ctx, None, None)
+        .await?;
+
+    let mut s = String::new();
+
+    for member in memberlist {
+        s.push_str(format!(
+            "nickname: {:?}, name: {}, discriminator: {}",
+            member.nick, member.user.name, member.user.discriminator
+        ));
+        s.push('\n')
+    }
+
+    utils::send_buffered_text(ctx, msg.channel_id, futures::stream::iter(s.trim().lines()));
+
+    Ok(())
 }
