b186651c2e6381b44653dd3a5ceb3e7ebec96dba
u296 <u296b3g@gmail.com>
2021-05-05 18:23:27 +0200

    added read_timer command

--- src/commands/general/timer.rs
+++ src/commands/general/timer.rs
@@ -1,6 +1,7 @@
 use super::prelude::*;
 
 #[command]
+#[aliases("reset_timer")]
 async fn start_timer(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
     ctx.data
         .write()
@@ -17,12 +18,39 @@ async fn start_timer(ctx: &Context, msg: &Message, _args: Args) -> CommandResult
 }
 
 #[command]
+async fn read_timer(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
+    match ctx
+        .data
+        .read()
+        .await
+        .get::<utils::TextChannelDataMap>()
+        .expect("no TextChannelDataMap in typemap")
+        .get(&msg.channel_id)
+        .unwrap_or(&Default::default())
+        .timer
+    {
+        Some(start) => {
+            let duration = Instant::now() - start;
+
+            msg.channel_id
+                .say(ctx, format!("{} s", duration.as_millis() as f32 / 1000.0))
+                .await?;
+        }
+        None => {
+            msg.channel_id.say(ctx, "no timer started").await?;
+        }
+    };
+
+    Ok(())
+}
+
+#[command]
 async fn stop_timer(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
     let typemap = &mut ctx.data.write().await;
 
     let timer = &mut typemap
         .get_mut::<utils::TextChannelDataMap>()
-        .expect("no TextChannelDataMaop in typemap")
+        .expect("no TextChannelDataMap in typemap")
         .entry(msg.channel_id)
         .or_default()
         .timer;
