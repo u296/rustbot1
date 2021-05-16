980ce06dcf6e9ef1abb664f73273e6b1f749ac1a
u296 <u296b3g@gmail.com>
2021-05-16 14:41:08 +0200

    added test

--- src/config.rs
+++ src/config.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 
 use serenity::prelude::*;
 
-#[derive(Deserialize)]
+#[derive(PartialEq, Clone, Debug, Deserialize)]
 pub struct Config {
     pub prefix: String,
     pub enable_exec: bool,
@@ -18,5 +18,24 @@ pub async fn read_config() -> Result<Config, Box<dyn Error>> {
     match serde_json::from_slice::<Config>(&config) {
         Ok(v) => Ok(v),
         Err(e) => Err(e.into()),
+    }
+}
+
+#[test]
+fn config_deserialize() {
+    let text = 
+r#"{
+    "prefix": ".",
+    "enable_exec": false
+}"#;
+
+    let wanted = Config {
+        prefix: String::from("."),
+        enable_exec: false
+    };
+
+    match serde_json::from_str::<Config>(text) {
+        Ok(c) => assert_eq!(c, wanted),
+        Err(_) => panic!("assertion failed")
     }
 }
