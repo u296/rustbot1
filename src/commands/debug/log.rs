a07f2f9c92bd0ce2688eebbc45b26917925a5d40
u296 <u296b3g@gmail.com>
2021-11-23 08:55:04 +0100

    remove hardcoded log path

--- src/commands/debug/log.rs
+++ src/commands/debug/log.rs
@@ -1,17 +1,27 @@
-use super::prelude::*;
+use std::path::Path;
 
-const LOG_FILE_LOCATION: &'static str = "/home/discord/logs/rustbot1/current";
-const LOG_FILE_TMP_STORE_LOCATION: &'static str = "/tmp/rustbot/log.txt";
-const LOG_FILE_TMP_STORE_DIR: &'static str = "/tmp/rustbot";
+use super::prelude::*;
+use crate::config::Config;
 
 #[command]
 async fn show_latest_log(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
-    tokio::fs::create_dir_all(LOG_FILE_TMP_STORE_DIR).await?;
-    tokio::fs::copy(LOG_FILE_LOCATION, LOG_FILE_TMP_STORE_LOCATION).await?;
-    utils::send_text_file(
-        ctx,
-        msg.channel_id,
-        vec![LOG_FILE_TMP_STORE_LOCATION].into_iter(),
-    )
-    .await
+    let log_file_location = ctx
+        .data
+        .read()
+        .await
+        .get::<Config>()
+        .expect("no config in typemap")
+        .log
+        .clone();
+
+    match log_file_location {
+        Some(file) => {
+            utils::send_text_file(ctx, msg.channel_id, vec![Path::new(&file)].into_iter()).await?;
+        }
+        None => {
+            msg.channel_id.say(ctx, "log file not configured").await?;
+        }
+    }
+
+    Ok(())
 }
